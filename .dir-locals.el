0b29d3ca18fdf743daa6ae5fa496d049c2ee4240
BloggerBust <trevor.wilson@bloggerbust.ca>
2019-07-06 16:10:19 -0600

    initial commit
    Just checking ignore files for git and projectile as well as my emacs
    dir locals script for working with python projects

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((projectile-indexing-method . native)
+         (projectile-enable-caching . t)
+         (projectile-test-command . "python -m unittest discover")
+         (python-check . "python -m pylint")
+         (projectile-project-run-cmd . "run.py")
+         (py-autopep8 t)
+         (py-autopep8-enable-on-save)
+         )))
